358da879450f196ffd73f0c1926efde08c3a6bf5
Gian Danovan <dodogian005@gmail.com>
2026-08-04 17:42:06 +0700

    Add main class to read and print input lines

---
+++ Java/Java End-of-file/main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nomor = 1;
+        while(sc.hasNextLine()){
+            System.out.println(nomor + " " + sc.nextLine());
+            nomor++;
+        }
+    }
+}
